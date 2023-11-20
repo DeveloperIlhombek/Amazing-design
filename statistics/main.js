bc940c58f64d06bc598176e0fd3389793e39fe20
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2023-11-20 23:37:32 +0500

    Counter up

---
+++ statistics/main.js
@@ -0,0 +1,16 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+
+valueDisplays.forEach((valueDisplay) => {
+  let starValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"))
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    starValue += 1;
+    valueDisplay.textContent = starValue;
+    if (starValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
